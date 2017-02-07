4e86bc14bee5c0e91415e69ab8f4dfba58519fe8
Ash Laidlaw and Charles Peden <charles@>
2017-02-07 15:48:50 -0800

    Add Twig refactor

--- app/app.php
+++ app/app.php
@@ -3,75 +3,21 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/JobOpening.php";
 
+
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
 
-    $app->get("/", function() {
-        return "
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-            <meta charset='utf-8'>
-            <title>Job Postings</title>
-          </head>
-          <body>
-            <div class='container'>
-              <h1 class='jumbotron'>Job Postings</h1>
-              <form action='/output'>
-                <div class='row'>
-                  <div class='col-sm-6'>
-                    <div class='form-group'>
-                      <label for='title'>Job Title:</label>
-                      <input type='text' name='title'>
-                    </div>
-
-                    <div class='form-group'>
-                      <label for='description'>Job Description:</label>
-                      <input type='text' name='description'>
-                    </div>
-                  </div>
-
-                  <div class='col-sm-6'>
-                    <div class='form-group'>
-                      <label for='contact'>Contact Info:</label>
-                      <input type='text' name='contact'>
-                    </div>
-                  </div>
-                </div>
-
-                <div class='row'>
-                  <div class='col-sm-12'>
-                    <button type='submit'>Post!</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </body>
-        </html>";
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('jobs.html.twig');
     });
 
-    $app->get("/output", function() {
+    $app->get("/output", function() use ($app) {
         $newPosting = new JobOpening($_GET['title'], $_GET['description'], $_GET['contact']);
-        $returnTitle = $newPosting->getTitle();
-        $returnDesc = $newPosting->getDescription();
-        $returnContact = $newPosting->getContact();
+        $variableName = "Ash";
 
-        return "
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
-            <meta charset='utf-8'>
-            <title>Job Postings</title>
-          </head>
-          <body>
-              <p>Your posted a new job! Here are the details:</p>
-              <ul>
-                  <li>$returnTitle</li>
-                  <li>$returnDesc</li>
-                  <li>$returnContact</li>
-              </ul>
-        ";
+        return $app['twig']->render('job_output.html.twig', array('newpost' => $newPosting));
     });
 
     return $app;
