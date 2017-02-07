ef55078661fc5ae2edb9ed444b765bd152b9c5d5
Ash Laidlaw and Charles Peden <charles@>
2017-02-07 10:31:15 -0800

    create JobOpening object w/ page and initialize index.php and app.php

---
+++ src/JobOpening.php
@@ -0,0 +1,14 @@
+<?php
+    class JobOpening {
+        private $title;
+        private $description;
+        private $contact_info;
+
+        function __construct($title, $description, $contact_info)
+        {
+            $this->title = $title;
+            $this->description = $description;
+            $this->contact_info = $contact_info;
+        }
+    }
+?>
